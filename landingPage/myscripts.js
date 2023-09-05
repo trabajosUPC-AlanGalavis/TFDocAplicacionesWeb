5b0e298c4037736691ce8f2abc3cb13bbf374d27
Andres (UPC) <103398708+asotito231@users.noreply.github.com>
2023-09-04 22:51:54 -0500

    feature: added language change option

--- landingPage/myscripts.js
+++ landingPage/myscripts.js
@@ -6,30 +6,61 @@ function toggleMobileMenu() {
     if (languageMenu.classList.contains('show-language-menu')) {
         languageMenu.classList.remove('show-language-menu');
     }
+
+    var navbarRight = document.querySelector('.navbar-right');
+    navbarRight.classList.toggle('show-mobile-menu');
 }
 
 function toggleLanguageMenu() {
     var languageMenu = document.querySelector('.language-menu');
     languageMenu.classList.toggle('show-language-menu');
+    
 }
 
-// Agregamos una función para cambiar el idioma
-function changeLanguage(lang) {
+
+function changeLanguage(lang, learn, benefits, about, plans, contact, log, create) {
     var languageSelector = document.querySelector('.language-selector');
     var languageIcon = languageSelector.querySelector('.fa');
+    var selectLearn = document.querySelector('.learn');
+    var selectBenefits = document.querySelector('.benefits');
+    var selectAbout = document.querySelector('.about-us');
+    var selectPlans = document.querySelector('.plans');
+    var selectContact = document.querySelector('.contact');
+    var selectLog = document.querySelector('.log-in');
+    var selectCreate =document.querySelector('.button-secondary');
     languageSelector.innerHTML = lang + ' ' + languageIcon.outerHTML;
-    toggleLanguageMenu(); // Ocultamos el menú de idiomas después de seleccionar uno
+    selectLearn.innerHTML = learn;
+    selectBenefits.innerHTML = benefits;
+    selectAbout.innerHTML = about;
+    selectPlans.innerHTML = plans;
+    selectContact.innerHTML = contact;
+    selectLog.innerHTML = log;
+    selectCreate.innerHTML = create;
+    toggleLanguageMenu();
 }
 
-// Event listener para cambiar el idioma cuando se hace clic en una opción
+
 document.addEventListener('click', function(event) {
-    var languageMenu = document.querySelector('.language-menu');
+    var esLearn = 'Conoce más';
+    var enLearn = 'Learn';
+    var esBenefits = 'Beneficios';
+    var enBenefits = 'Benefits';
+    var esAbout = 'Acerca de';
+    var enAbout = 'About us';
+    var esPlans = 'Planes';
+    var enPlans = 'Plans';
+    var esContact = 'Contacto';
+    var enContact = 'Contact';
+    var esLog = 'Iniciar sesión';
+    var enLog = 'Log in';
+    var esCreate = 'Crear cuenta';
+    var enCreate = 'Create account';
     if (event.target.classList.contains('language-option')) {
         var selectedLanguage = event.target.textContent;
-        changeLanguage(selectedLanguage);
-    } else if (!event.target.closest('.language-menu') && !event.target.closest('.language-selector')) {
-        if (languageMenu.classList.contains('show-language-menu')) {
-            languageMenu.classList.remove('show-language-menu');
+        if (selectedLanguage == 'EN'){
+            changeLanguage(selectedLanguage, enLearn, enBenefits, enAbout, enPlans, enContact, enLog, enCreate);
+        } else if (selectedLanguage == 'ES'){
+            changeLanguage(selectedLanguage, esLearn, esBenefits, esAbout, esPlans, esContact, esLog, esCreate);
         }
-    }
+    } 
 });
